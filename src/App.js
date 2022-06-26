37d111435c75e35f8586eca1e9443b428976f983
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-26 12:45:27 +0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./Stylings/style.css";
 import Routing from "./Routing/Routing";
+import Footer from "./Footer";
+
 function App() {
   return (
     <div>
       <Routing />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+
+    return (
+        <div>
+            <footer>
+                <p>&copy; 2020 <a href="https://umairahmad.net">www.umairahmad.net</a>.All right Reserved </p>
+            </footer>
+        </div>
+
+    )
+}
