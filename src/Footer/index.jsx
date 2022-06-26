7103b2911048885743acbbf0ee1a99b0ddb0aeca
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-26 22:46:20 +0500

    Update index.jsx

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 export default function Footer() {
-
     return (
         <footer>
             <p>&copy; 2020 <a href="https://umairahmad.net">www.umairahmad.net</a>.All right Reserved </p>
